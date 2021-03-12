18a6a714eb949e68541454a7b2064349932879b8
Capral-Levi <79367525+Capral-Levi@users.noreply.github.com>
2021-03-12 21:36:40 +0300

    feat: fix bags

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 
 exports.min = function min (array) {
   if (array === undefined) {
-    return 0;}
+    return 0;
+  }
   if (array.length === 0) {
-    return 0;}
+    return 0;
+  }
   return Math.min.apply(Math,array);
   
 }
 
 exports.max = function max (array) {
   if (array === undefined) {
-    return 0;}
+    return 0;
+  }
   if (array.length === 0) {
-    return 0;}
+    return 0;
+  }
   return Math.max.apply(Math,array);
 }
 
 exports.avg = function avg (array) {
   if (array === undefined) {
-    return 0;}
+    return 0;
+  }
   if (array.length === 0) {
     return 0;
   }
   
-  let res = 0
-  for (let i=0; i < array.length;i++){
-    res += array[i];} 
-    return res/array.length;}
+  var res = 0
+  for (var i=0; i < array.length;i++){
+    res += array[i];
+  } 
+    return res/array.length;
+}
